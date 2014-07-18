baed87ebfaf14b2f42a9b1344edb160ae7bf54a3
DanAhn <danielahn22@hanmail.net>
2014-07-18 14:16:58 +0900

    test

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<HTML>
+	<HEAD>
+		<TITLE>검색 결과테ㄴ스트 </TITLE>
+		<meta charset="utf=-8">
+		<link href="/usr/local/apache/htdocs/danAhn/phpstud/board_test/css/css/bootstrap.css" rel="stylesheet">
+	</HEAD>
+	<BODY>
+		<div class="header">
+		</div>
+		<div class="content">
